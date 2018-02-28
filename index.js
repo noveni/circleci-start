9c92c430c20f4c244f8d0efc95f6218ab240afbe
Nicolas Novello <nico.novello@gmail.com>
2018-02-28 11:04:56 +0100

    updated repo

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'dist')))
-  .get('/', (req, res) => res.render('pages/index'))
+  // .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
